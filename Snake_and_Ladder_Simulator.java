9a197d28cf8d5cba8853f64bdd985d515a355ce7
kirankumar-Matham96 <mathamkirankumar96@gmail.com>
2021-03-23 13:56:11 +0530

    ADD: Generating options-NO_Play/Ladder/Snake

--- Snake_and_Ladder_Simulator.java
+++ Snake_and_Ladder_Simulator.java
@@ -5,6 +5,7 @@ public class Snake_and_Ladder_Simulator
 		//Constants
 		final int START_POSITION = 0;
 		final int WINNING_POSITION = 100;
+		final int NO_PLAY = 0;
 		final int LADDER = 1;
 		final int SNAKE = 2;
 
@@ -15,6 +16,9 @@ public class Snake_and_Ladder_Simulator
 		//Start the game
 		//->Rolling a Die
 		int dieRoll = (int) (1+Math.random()*10%6);
+
+		//->Generating options for game
+		int option = (int) (Math.random()*10%3);
 
 	}
 }
