9ce9252b5d100deda2113d770bd75d5ef39b6c24
kirankumar-Matham96 <mathamkirankumar96@gmail.com>
2021-03-23 13:44:04 +0530

    ADD: Player starts the game with zero position

--- Snake_and_Ladder_Simulator.java
+++ Snake_and_Ladder_Simulator.java
@@ -2,6 +2,7 @@ public class Snake_and_Ladder_Simulator
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Welcome to the Snake & Ladder Simulator!");
+		//variables
+		int position = 0; //Player's initial position
 	}
 }
